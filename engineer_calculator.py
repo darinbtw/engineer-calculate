4fdb77ed93c28d8d05cfe4dca54511d01fb5946f
darinbtw <ruakhim@bk.ru>
2023-11-28 13:05:17 +0300

    update engeneer_calculator.py

--- engineer_calculator.py
+++ engineer_calculator.py
@@ -1,18 +1,68 @@
 from tkinter import Tk, END, messagebox, ttk, W, E, N, S
-
 import math
+
+def calc(key):
+    try:
+        if key == '=':
+            expression = calc_entry.get()
+            # Replace '÷' with '/'
+            expression = expression.replace('÷', '/')
+            result = eval(expression)
+            calc_entry.delete(0, END)
+            calc_entry.insert(END, str(result))
+        elif key in {'cos', 'sin', 'log', 'ln', 'n!', 'e', 'π', '√'}:
+            # Unary operations
+            expression = calc_entry.get()
+            result = perform_unary_operation(key, expression)
+            calc_entry.delete(0, END)
+            calc_entry.insert(END, str(result))
+        elif key == 'C':
+            calc_entry.delete(0, END)
+        elif key == 'Exit':
+            root.after(1, root.destroy)
+        else:
+            # For other keys, insert them into the entry
+            calc_entry.insert(END, key)
+    except (ValueError, ZeroDivisionError, OverflowError) as e:
+        calc_entry.delete(0, END)
+        calc_entry.insert(END, 'Error')
+        messagebox.showerror('Error', str(e))
+
+def perform_unary_operation(operation, value):
+    if operation == 'cos':
+        return math.cos(float(value))
+    elif operation == 'sin':
+        return math.sin(float(value))
+    elif operation == 'log':
+        return math.log10(float(value))
+    elif operation == 'ln':
+        return math.log(float(value))
+    elif operation == 'n!':
+        return math.factorial(int(value))
+    elif operation == 'e':
+        return math.e
+    elif operation == 'π':
+        return math.pi
+    elif operation == '√':
+        return math.sqrt(float(value))
 
 root = Tk()
 root.title('Инженерный калькулятор')
+root.configure(bg='#CCCCCC')  #Gray background
 
-# Устанавливаем веса для автоматического изменения размеров при изменении окна
+# Styling
+style = ttk.Style()
+style.configure("TButton", padding=(10, 5), font=('Arial', 12))
+style.configure("TEntry", font=('Arial', 14))
+
+# Window Resizing
 for i in range(5):
     root.columnconfigure(i, weight=1)
     root.rowconfigure(i, weight=1)
 
-# Устанавливаем минимальный размер окна
 root.minsize(width=400, height=500)
 
+# Buttons
 btn_list = [
     'cos', 'sin', 'log', 'ln', 'n!',
     'e', 'π', '√', '+', '*',
@@ -22,10 +72,8 @@ btn_list = [
     '0', '.', '=', 'C', 'Exit'
 ]
 
-calc_entry = ttk.Entry(root, width=33, font=('Arial', 14))
+calc_entry = ttk.Entry(root, width=33)
 calc_entry.grid(row=0, column=0, columnspan=5, sticky=W + E + N + S, pady=10)
-
-ttk.Style().configure("TButton", padding=(0, 10, 0, 10), font=('Arial', 12))
 
 r = 1
 c = 0
@@ -33,52 +81,10 @@ for btn in btn_list:
     rel = ''
     cmd = lambda x=btn: calc(x)
 
-    ttk.Button(root, text=btn, command=cmd, width=15).grid(row=r, column=c, pady=5, sticky=W + E + N + S)
+    ttk.Button(root, text=btn, command=cmd).grid(row=r, column=c, pady=5, sticky=W + E + N + S)
     c += 1
     if c > 4:
         c = 0
         r += 1
-
-def calc(key):
-    if key == '=':
-        try:
-            expression = calc_entry.get()
-            result = eval(expression)
-            calc_entry.delete(0, END)
-            calc_entry.insert(END, str(result))
-        except ValueError:
-            calc_entry.delete(0, END)
-            calc_entry.insert(END, 'Ошибка!')
-            messagebox.showerror('Ошибка! Проверьте введенные данные.')
-    elif key == 'cos':
-        calc_entry.insert(END, "=" + str(math.cos(int(calc_entry.get()))))
-    elif key == 'sin':
-        calc_entry.insert(END, "=" + str(math.sin(int(calc_entry.get()))))
-    elif key == 'log':
-        calc_entry.insert(END, "=" + str(math.log(int(calc_entry.get()))))
-    elif key == 'ln':
-        calc_entry.insert(END, "=" + str(math.log(int(calc_entry.get()), math.e)))
-    elif key == 'n!':
-        calc_entry.insert(END, "=" + str(math.factorial(int(calc_entry.get()))))
-    elif key == 'e':
-        calc_entry.insert(END, math.e)
-    elif key == 'π':
-        calc_entry.insert(END, math.pi)
-    elif key == '√':
-        calc_entry.insert(END, '=' + str(math.sqrt(int(calc_entry.get()))))
-    elif key == '÷':
-        calc_entry.insert(END, '/')
-    elif key == 'xⁿ':
-        calc_entry.insert(END, '**')
-    elif key == '(':
-        calc_entry.insert(END, '(')
-    elif key == ')':
-        calc_entry.insert(END, ')')
-    elif key == 'C':
-        calc_entry.delete(0, END)
-    elif key == 'Exit':
-        root.after(1, root.destroy)
-    else:
-        calc_entry.insert(END, key)
-
+#start
 root.mainloop()
